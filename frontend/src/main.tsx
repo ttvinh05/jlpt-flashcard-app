e85403433cdf7086a2a4286ccac9fc90b94d206e
Vinh <23521799@gm.uit.edu.vn>
2026-06-24 16:49:02 +0700

    chore: fix setup TypeScript and migrate main.jsx to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { TooltipProvider } from "./components/ui/tooltip";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TooltipProvider>
       <App />
