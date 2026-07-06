aedd67c7d80a036353426c3f99306d17831434cb
Vinh <23521799@gm.uit.edu.vn>
2026-07-06 22:16:28 +0700

    chore: migrate supabase and utils to typescript

---
+++ frontend/src/lib/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error(
+    "Lỗi: Thiếu hoặc cấu hình sai biến môi trường trong file .env!",
+  );
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
